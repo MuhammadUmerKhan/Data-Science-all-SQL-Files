9f2b1d551616e7e54cc6c7721985edf210511626
MuhammadUmerKhan <muhammadumerk546@gmail.com>
2024-02-12 15:32:09 +0500

    Class 4

--- sanFransisco.sql
+++ sanFransisco.sql
@@ -1,1 +1,19 @@
+-- CREATE DATABASE 
+
+CREATE TABLE Movies (
+    Title VARCHAR(255),
+    Release_Year INT,
+    Locations VARCHAR(255),
+    Fun_Facts TEXT,
+    Production_Company VARCHAR(255),
+    Distributor VARCHAR(255),
+    Director VARCHAR(255),
+    Writer VARCHAR(255),
+    Actor_1 VARCHAR(255),
+    Actor_2 VARCHAR(255),
+    Actor_3 VARCHAR(255),
+    SF_Find_Neighborhoods VARCHAR(255),
+    Analysis_Neighborhoods VARCHAR(255),
+    Current_Supervisor_Districts INT
+);
 SELECT * FROM movies;
